1e976073f4b89c7b28c7c479e8c67ff2a4f9c290
SJ <weesj96@gmail.com>
2023-04-10 23:41:21 +0800

    Update answers.txt

--- exercise-02/src/lib.rs
+++ exercise-02/src/lib.rs
@@ -8,13 +8,49 @@ pub struct Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
+    let mut total: u32 = 0;
+
     for line in contents.lines() {
-        println!("{}", line);
+      let vector: Vec<&str> = line.split_whitespace().collect();
+      let opponent = vector[0];
+      let mine = vector[1];
+      
+      total += get_outcome(opponent, mine) + get_shape_score(mine);
     }
 
-    // println!("{}", );
+    println!("{}", total);
 
     Ok(())
+}
+
+fn get_outcome(opponent: &str, mine: &str) -> u32 {
+    match opponent {
+        "A" => match mine { 
+            "X" => 3,
+            "Y" => 6,
+            _ => 0,
+        },
+        "B" => match mine { 
+            "Y" => 3,
+            "Z" => 6,
+            _ => 0,
+        },
+        "C" => match mine { 
+            "Z" => 3,
+            "X" => 6,
+            _ => 0,
+        },
+        _ => 0,
+    }
+}
+
+fn get_shape_score(shape: &str) -> u32 {
+    match shape {
+        "X" => 1,
+        "Y" => 2,
+        "Z" => 3,
+        _ => 0 // put as zero first might do some error handling next time
+    }
 }
 
 impl Config {
